3b748d0c8388ffcd6149220032d447920981dc9a
Noah Pistilli <noah@wiilink.ca>
2025-06-25 10:53:44 -0400

    chore: Remove unused structures

--- structure.go
+++ structure.go
@@ -3,16 +3,8 @@ package main
 import (
 	"encoding/xml"
 	"github.com/coreos/go-oidc/v3/oidc"
-	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/oauth2"
 )
-
-type JWTClaims struct {
-	Username string `json:"nickname"`
-	PFP      string `json:"picture"`
-	Email    string
-	jwt.RegisteredClaims
-}
 
 type OIDCConfig struct {
 	XMLName      xml.Name `xml:"oidc"`
@@ -24,16 +16,9 @@ type OIDCConfig struct {
 	LogoutURL    string   `xml:"logoutURL"`
 }
 
-type DiscordOAuthConfig struct {
-	ClientID     string `xml:"clientID"`
-	ClientSecret string `xml:"clientSecret"`
-	RedirectURL  string `xml:"redirectURL"`
-}
-
 type Config struct {
-	Address            string             `xml:"address"`
-	OIDCConfig         OIDCConfig         `xml:"oidc"`
-	DiscordOAuthConfig DiscordOAuthConfig `xml:"discord"`
+	Address    string     `xml:"address"`
+	OIDCConfig OIDCConfig `xml:"oidc"`
 }
 
 type AppAuthConfig struct {
